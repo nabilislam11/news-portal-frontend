3b078b5a9a97a7cc27de0169abbfe777f23d0376
nabilislam11 <hamjanabil11@gmail.com>
2025-12-26 07:04:26 +0600

    aslfjasfjkljflkasjf klaf

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,4 @@
-import { StrictMode } from "react";
+//
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
@@ -11,10 +11,10 @@ const authConfig = {
 };
 
 createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <AuthProvider config={authConfig}>
-      <App />
-      <Toaster />
-    </AuthProvider>
-  </StrictMode>
+  // <StrictMode>
+  <AuthProvider config={authConfig}>
+    <App />
+    <Toaster />
+  </AuthProvider>
+  // </StrictMode>
 );
